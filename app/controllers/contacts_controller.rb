3fc62a9ab579afd953ef507db446eebf42979f7d
Dominic Henry <easycrom@gmail.com>
2015-04-06 21:01:19 +0000

    Added ability to save contacts to the Contacts controller.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,9 +2,22 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
-  end
+  end #End of creating a new "Contact" object
   
   def create
-  end
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message sent."
+    else
+      redirect_to new_contact_path, notice: "Error occured."
+    end
+  end #End of create new contact action
   
-end
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end#End of private section of code
+  
+  
+end #End of Contacts controller
