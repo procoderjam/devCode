1434607d7720679ebd66cdbb96ee86caeaa5472c
Dominic Henry <easycrom@gmail.com>
2015-04-09 02:10:28 +0000

    Added validations and create action and flash error messages.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,16 +8,18 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Error occured."
+      flash[:danger] = 'Error occured message has not been sent!'
+      redirect_to new_contact_path
     end
   end #End of create new contact action
   
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
-    end#End of private section of code
+    end #End of private section of code
   
   
 end #End of Contacts controller
